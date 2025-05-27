41191661866b40a9a42a7eb282ab8cec67d52bec
Henry Li <henryli@eduroam-campus-10-36-235-187.campus-dynamic.uwaterloo.ca>
2025-05-27 15:09:47 -0400

    info.yaml

--- src/project.v
+++ src/project.v
@@ -49,8 +49,8 @@ module tt_um_onboarding_HenryLi (
 	// All output pins must be assigned. If not used, assign to 0.
 	assign uio_oe = 8'hFF;
 	//	assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-	//	assign uio_out = 0;
-	//	assign uio_oe  = 0;
+	assign uo_out  = en_reg_out_7_0;
+	assign uio_out = en_reg_out_15_8;
 
 
 	// List all unused inputs to prevent warnings
